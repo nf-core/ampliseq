114a5270f0578066cb5cfed31698a533ffa0f84b
d4straub <daniel.straub@uni-tuebingen.de>
2018-10-23 15:48:26 +0200

    add dada_trunc_parameter.py

--- bin/dada_trunc_parameter.py
+++ bin/dada_trunc_parameter.py
@@ -1,11 +1,34 @@
 #!/usr/bin/env python3
-#@author Alexander Peltzer
-# Takes multiple CSV files and generates a tuple of index locations > that resemble the cutoff value used for DADA2 in QIIME2.
+#@author Daniel Straub
+# Takes two CSV files from QIIME2 demux output and a quality threshold 
+# to generate a tuple of index locations that resemble the cutoff value used for DADA2 in QIIME2.
 
 import pandas as pd
 import sys 
 
+#argument check
+if len(sys.argv) != 4:
+    exit("Usage: dada_trunc_parameter.py <forward-seven-number-summaries.csv> <reverse-seven-number-summaries.csv> <int>")
 
-data_fw = pd.read_csv(sys.argv[1]) 
-data_rv = pd.read_csv(sys.argv[2])
+#parameters
+data_fw = pd.read_csv(sys.argv[1]) #quality values forward reads
+data_rv = pd.read_csv(sys.argv[2]) #quality values reverse reads
+qmin = float(sys.argv[3]) #threshold
 
+#select row with median values (file row 6, starting with "50%") and drop first row
+median_fw = data_fw.iloc[4][1:].values.tolist()
+median_rv = data_rv.iloc[4][1:].values.tolist()
+
+#iterate through values and find first value that falls below threshold
+def function(median):
+    trunc = len(median)
+    for value in median:
+        if value < qmin:
+            trunc = median.index(value)
+            break
+    return trunc
+trunc_fw = function(median_fw)
+trunc_rv = function(median_rv)
+
+#print values
+print(trunc_fw, trunc_rv, sep=',', end='')

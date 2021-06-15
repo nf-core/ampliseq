df3c580933f2418963e6fb9f11c30ef92e2d668b
Daniel Lundin <erik.rikard.daniel@gmail.com>
2021-06-15 15:53:00 +0200

    Fixed spelling mistake

--- bin/taxref_reformat_gtdb.sh
+++ bin/taxref_reformat_gtdb.sh
@@ -12,5 +12,5 @@ done
 # Write the assignTaxonomy() fasta file: assignTaxonomy.fna
 cat ar122*.fna bac120*.fna | sed '/^>/s/>\([^ ]\+\) \([^[]\+\) \[.*/>\2(\1\)/' | sed '/^>/s/;s__.*//' | sed 's/[a-z]__//g' | sed 's/ /_/g' > assignTaxonomy.fna
 
-# Write the assignTaxonomy() fasta file: addSpecies.fna
+# Write the addSpecies() fasta file: addSpecies.fna
 cat ar122*.fna bac120*.fna | sed '/^>/s/>\([^ ]\+\) .*;s__\([^[]\+\) \[.*/>\1 \2/' > addSpecies.fna

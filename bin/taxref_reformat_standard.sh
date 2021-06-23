64a2385dcc5c3d16e0c55d768a6801527e46a6fb
Daniel Lundin <matricaria.suaveolens@gmail.com>
2021-06-23 18:12:51 +0200

    Got rid of extra ; that shifted taxonomy

--- bin/taxref_reformat_standard.sh
+++ bin/taxref_reformat_standard.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-# Uses preformatted databses from DADA2 (https://benjjneb.github.io/dada2/training.html)
+# Uses preformatted databases from DADA2 (https://benjjneb.github.io/dada2/training.html)
 # The file for taxonomy assignment, identified by containing "train" in the name,
-# and the file for add species, identified by containing "species" in the name, are renamed 
+# gets the first field duplicated:
+gunzip -c *train*gz | sed 's/>\([^;]*\)/>\1;\1/' > assignTaxonomy.fna
 
-gunzip -c *train*gz | sed 's/>\([^;]*\)/>\1;\1;/' > assignTaxonomy.fna
+# and the file for add species, identified by containing "species" in the name, is renamed 
 mv *species*gz addSpecies.fna.gz

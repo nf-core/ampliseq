381b3dc16a6497f125c1cba67934e11804d21208
Daniel Lundin <matricaria.suaveolens@gmail.com>
2021-08-06 10:09:40 +0200

    Four space indentation

--- bin/taxref_reformat_sbdi-gtdb.sh
+++ bin/taxref_reformat_sbdi-gtdb.sh
@@ -7,11 +7,11 @@
 # names.
 
 for f in *; do
-  if [ $(file -L $f | grep -c 'gzip') ]; then
-    if [ $(gunzip -c $f | head -n 1 | grep -c '>.*;.*;.*;.*;.*') -eq 1 ]; then
-      gunzip -c $f > assignTaxonomy.fna
-    elif [ $(gunzip -c $f | head -n 1 | grep -c '>.* .*') -eq 1 ]; then
-      gunzip -c $f > addSpecies.fna
+    if [ $(file -L $f | grep -c 'gzip') ]; then
+        if [ $(gunzip -c $f | head -n 1 | grep -c '>.*;.*;.*;.*;.*') -eq 1 ]; then
+            gunzip -c $f > assignTaxonomy.fna
+        elif [ $(gunzip -c $f | head -n 1 | grep -c '>.* .*') -eq 1 ]; then
+            gunzip -c $f > addSpecies.fna
+        fi
     fi
-  fi
 done

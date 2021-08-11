42aa99c5bfd52ea1e07e9f58fbedd7e89c86cc41
Daniel Lundin <matricaria.suaveolens@gmail.com>
2021-08-11 20:45:01 +0200

    Small changes in SBDI export format

--- bin/sbdiexport.R
+++ bin/sbdiexport.R
@@ -15,31 +15,6 @@
 
 suppressPackageStartupMessages(library(tidyverse))
 
-# Basename for outfiles
-BASENAME = "SBDI-metabarcoding-template"
-
-# Missing params that we might ask for, otherwise provide default values
-sop                     <- "STANDARD OPERATING PROCEDURE"
-target_gene             <- "TARGET GENE"
-target_subfragment      <- ""
-pcr_primer_name_forward <- "FWD PRIMER NAME"
-pcr_primer_name_reverse <- "REV PRIMER NAME"
-env_broad_scale         <- "SEE MIXS SPEC"
-env_local_scale         <- "SEE MIXS SPEC"
-env_medium              <- "SEE MIXS SPEC"
-event_id_alias          <- ""
-measurementType         <- ""
-measurementTypeID       <- ""
-measurementValue        <- ""
-measurementValueID      <- ""
-measurementUnit         <- ""
-measurementUnitID       <- ""
-measurementAccuracy     <- ""
-measurementDeterminedDate <- ""
-measurementDeterminedBy <- ""
-measurementMethod       <- ""
-measurementRemarks      <- ""
-
 # Get the library layout and primers from the command line
 args = commandArgs(trailingOnly=TRUE)
 lib_layout      <- args[1]
@@ -55,55 +30,72 @@ taxonomy <- read.delim("ASV_tax_species.tsv", sep = '\t')
 
 # Write the event tab
 data.frame(
-  'event_id_alias' = colnames(asvs)[2:(n_samples+1)], 'materialSampleID' = character(n_samples),
-  'eventDate' = character(n_samples), 'samplingProtocol' = character(n_samples),
-  'sampleSizeValue' = character(n_samples), 'locationID' = character(n_samples),
-  'decimalLatitude' = character(n_samples), 'decimalLongitude' = character(n_samples),
-  'geodeticDatum' = character(n_samples), 'coordinateUncertaintyInMeters' = character(n_samples),
-  'recordedBy' = character(n_samples), 'country' = character(n_samples),
-  'municipality' = character(n_samples), 'verbatimLocality' = character(n_samples),
-  'minimumElevationInMeters' = character(n_samples), 'maximumElevationInMeters' = character(n_samples),
-  'minimumDepthInMeters' = character(n_samples), 'maximumDepthInMeters' = character(n_samples)
+  'event_id_alias' = colnames(asvs)[2:(n_samples+1)],
+  'materialSampleID' = character(n_samples),
+  'eventDate' = character(n_samples),
+  'samplingProtocol' = character(n_samples),
+  'locationID' = character(n_samples),
+  'decimalLatitude' = character(n_samples),
+  'decimalLongitude' = character(n_samples),
+  'geodeticDatum' = character(n_samples),
+  'coordinateUncertaintyInMeters' = character(n_samples),
+  'recordedBy' = character(n_samples),
+  'country' = character(n_samples),
+  'municipality' = character(n_samples),
+  'verbatimLocality' = character(n_samples),
+  'minimumElevationInMeters' = character(n_samples),
+  'maximumElevationInMeters' = character(n_samples),
+  'minimumDepthInMeters' = character(n_samples),
+  'maximumDepthInMeters' = character(n_samples)
 ) %>%
-  write_tsv(sprintf("%s.event.tsv", BASENAME))
+  inner_join(
+    asvs %>% pivot_longer(2:ncol(.), names_to = 'event_id_alias', values_to = 'count') %>%
+      group_by(event_id_alias) %>% summarise(sampleSizeValue = sum(count), .groups = 'drop'),
+    by = 'event_id_alias'
+  ) %>%
+  select(1:4, sampleSizeValue, 5:17) %>%
+  write_tsv("event.tsv", na = '')
 
 # mixs
 data.frame(
   'event_id_alias' = colnames(asvs)[2:(n_samples+1)],
-  'sop' = rep(sop, n_samples),
-  'target_gene' = rep(target_gene, n_samples),
-  'target_subfragment' = rep(target_subfragment, n_samples),
-  'lib_layout' = rep(lib_layout, n_samples), 
-  'pcr_primer_name_forward' = rep(pcr_primer_name_forward, n_samples),
-  'pcr_primer_name_reverse' = rep(pcr_primer_name_reverse, n_samples),
-  'pcr_primer_forward' = rep(fwd_primer_seq, n_samples),
-  'pcr_primer_reverse' = rep(rev_primer_seq, n_samples), 
-  'env_broad_scale' = rep(env_broad_scale, n_samples),
-  'env_local_scale' = rep(env_local_scale, n_samples),
-  'env_medium' = rep(env_medium, n_samples)
+  'sop' = character(n_samples),
+  'target_gene' = character(n_samples),
+  'target_subfragment' = character(n_samples),
+  'lib_layout' = character(n_samples),
+  'pcr_primer_name_forward' = character(n_samples),
+  'pcr_primer_name_reverse' = character(n_samples),
+  'pcr_primer_forward' = character(n_samples),
+  'pcr_primer_reverse' = character(n_samples),
+  'env_broad_scale' = character(n_samples),
+  'env_local_scale' = character(n_samples),
+  'env_medium' = character(n_samples)
 ) %>%
-  write_tsv(sprintf("%s.mixs.tsv", BASENAME))
+  write_tsv("mixs.tsv", na = '')
 
 # emof
 data.frame(
-  'event_id_alias' = colnames(asvs)[2:(n_samples+1)],
-  'measurementType' = rep(measurementType, n_samples),
-  'measurementTypeID' = rep(measurementTypeID, n_samples),
-  'measurementValue' = rep(measurementValue, n_samples),
-  'measurementValueID' = rep(measurementValueID, n_samples),
-  'measurementUnit' = rep(measurementUnit, n_samples),
-  'measurementUnitID' = rep(measurementUnitID, n_samples),
-  'measurementAccuracy' = rep(measurementAccuracy, n_samples),
-  'measurementDeterminedDate' = rep(measurementDeterminedDate, n_samples),
-  'measurementDeterminedBy' = rep(measurementDeterminedBy, n_samples),
-  'measurementMethod' = rep(measurementMethod, n_samples),
-  'measurementRemarks' = rep(measurementRemarks, n_samples)
+  'event_id_alias' = character(n_samples),
+  'measurementType' = character(n_samples),
+  'measurementTypeID' = character(n_samples),
+  'measurementValue' = character(n_samples),
+  'measurementValueID' = character(n_samples),
+  'measurementUnit' = character(n_samples),
+  'measurementUnitID' = character(n_samples),
+  'measurementAccuracy' = character(n_samples),
+  'measurementDeterminedDate' = character(n_samples),
+  'measurementDeterminedBy' = character(n_samples),
+  'measurementMethod' = character(n_samples),
+  'measurementRemarks' = character(n_samples)
 ) %>%
-  write_tsv(sprintf("%s.emof.tsv", BASENAME))
+  write_tsv("emof.tsv", na = '')
 
 # asv-table
 asvtax   <- asvs %>% 
   inner_join(taxonomy, by = 'ASV_ID') %>%
   rename(asv_id_alias = ASV_ID, DNA_sequence = sequence) %>%
   rename_with(tolower, Domain:Species) %>%
-  write_tsv(sprintf("%s.asv-table.tsv", BASENAME))
+  rename(specificEpithet = species) %>%
+  select(-confidence) %>%
+  mutate(associatedSequences = '', domain = str_remove(domain, 'Reversed:_')) %>%
+  write_tsv("asv-table.tsv", na = '')

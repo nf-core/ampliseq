d4796e79ab63f2af7afe0b73ade2ee5be9b87401
d4straub <daniel.straub@uni-tuebingen.de>
2022-07-26 17:30:49 +0200

    rename to midori2

--- bin/taxref_reformat_midori2.sh
+++ bin/taxref_reformat_midori2.sh
@@ -3,10 +3,10 @@
 # Handles the MIDORI database.
 
 # There are preformatted DADA2 files for assignTaxonomy() and addSpecies() -- this is just ungzipped
-for f in MIDORI_UNIQ_NUC_*_DADA2.fasta.gz; do
-    if [[ $f == *"MIDORI_UNIQ_NUC_SP_"* ]]; then
+for f in MIDORI2_UNIQ_NUC_*_DADA2.fasta.gz; do
+    if [[ $f == *"MIDORI2_UNIQ_NUC_SP_"* ]]; then
         gunzip -c $f > addSpecies.fna
-    elif [[ $f == *"MIDORI_UNIQ_NUC_"* ]]; then
+    elif [[ $f == *"MIDORI2_UNIQ_NUC_"* ]]; then
         gunzip -c $f > assignTaxonomy.fna
     fi
 done

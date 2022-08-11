81168768b1d0373547b493abf91c0126e908bcc0
d4straub <daniel.straub@uni-tuebingen.de>
2022-08-11 14:15:29 +0200

    revert duplicating tax levels

--- bin/taxref_reformat_midori2.sh
+++ bin/taxref_reformat_midori2.sh
@@ -7,7 +7,6 @@ for f in MIDORI2_UNIQ_NUC_*_DADA2.fasta.gz; do
     if [[ $f == *"MIDORI2_UNIQ_NUC_SP_"* ]]; then
         gunzip -c $f > addSpecies.fna
     elif [[ $f == *"MIDORI2_UNIQ_NUC_"* ]]; then
-        # gets the first field duplicated:
-        gunzip -c $f | sed 's/>\([^;]*\)/>\1;\1/' > assignTaxonomy.fna
+        gunzip -c $f > assignTaxonomy.fna
     fi
 done

2feaebcbc04c8bebc6f59dfcbfc482c4d75b9595
d4straub <daniel.straub@uni-tuebingen.de>
2022-09-01 17:09:12 +0200

    run black

--- bin/add_sh_to_taxonomy.py
+++ bin/add_sh_to_taxonomy.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#@author Jeanette Tångrot
+# @author Jeanette Tångrot
 
 # Adds UNITE species hypothesis (SH) information to ASV table based on vsearch usearch_global results in blast6 format.
 #
@@ -22,29 +22,29 @@ if len(sys.argv) != 6:
 outfile = sys.argv[5]
 
 # Read sequence to SH matchings
-seq2sh = pd.read_csv(sys.argv[1], sep='\t', header=None, index_col=0, skiprows=None, compression='bz2')
+seq2sh = pd.read_csv(sys.argv[1], sep="\t", header=None, index_col=0, skiprows=None, compression="bz2")
 
 # Read SH taxonomies
 # Columns:
 # SH  taxonid  kingdom  phylum  class  order  family  genus  species
-shtax = pd.read_csv(sys.argv[2], sep='\t', header=None, index_col=0, skiprows=None, compression='bz2')
+shtax = pd.read_csv(sys.argv[2], sep="\t", header=None, index_col=0, skiprows=None, compression="bz2")
 # Replace taxonid with Domain = "Eukaryota"
-shtax.loc[:,1] = 'Eukaryota'
+shtax.loc[:, 1] = "Eukaryota"
 # Remove genus from species name
-shtax.loc[:,8] = shtax.loc[:,8].str.split(" ",1).str[1]
+shtax.loc[:, 8] = shtax.loc[:, 8].str.split(" ", 1).str[1]
 
 # Read taxonomy table
 # Determine number of taxonomy levels from header
 # ASV_ID  Domain  Kingdom Phylum  Class   Order   Family  Genus   confidence      sequence
-taxtable = pd.read_csv(sys.argv[3], sep='\t', header=0)
+taxtable = pd.read_csv(sys.argv[3], sep="\t", header=0)
 num_ranks = len(taxtable.columns) - 3
 # Add SH slot to table:
 # ASV_ID  Domain  Kingdom Phylum  Class   Order   Family  Genus  SH confidence      sequence
-taxtable.insert(num_ranks+1,"SH","", allow_duplicates=False)
-tax_entries = list(taxtable.columns)[1:num_ranks+3]
+taxtable.insert(num_ranks + 1, "SH", "", allow_duplicates=False)
+tax_entries = list(taxtable.columns)[1 : num_ranks + 3]
 
 # Go through vsearch matches and update taxonomy for those entries
-fh = open( sys.argv[4], mode = 'r' )
+fh = open(sys.argv[4], mode="r")
 prev_ASV = fh.readline().split()[0]
 fh.seek(0)
 matches = []
@@ -60,30 +60,30 @@ for row in fh:
         tax = ""
         conf = 0.0
         for m in matches:
-            matchparts = m[0].split('|')
+            matchparts = m[0].split("|")
             try:
-                new_SH = seq2sh.loc[ matchparts[1] ][1]
+                new_SH = seq2sh.loc[matchparts[1]][1]
             except KeyError:
-                print( "WARNING: " + matchparts[1] + " not in seq2SH list", file=sys.stderr )
+                print("WARNING: " + matchparts[1] + " not in seq2SH list", file=sys.stderr)
                 new_SH = ""
-            if ( pd.isna( new_SH ) ) :
-                print( "WARNING: no SH reported for " + matchparts[1], file=sys.stderr )
+            if pd.isna(new_SH):
+                print("WARNING: no SH reported for " + matchparts[1], file=sys.stderr)
                 new_SH = ""
-            if SH != "" and new_SH != SH :
+            if SH != "" and new_SH != SH:
                 SH = ""
                 tax = ""
                 break
             elif new_SH != "":
                 SH = new_SH
                 try:
-                    tax = list(shtax.loc[ SH ])
+                    tax = list(shtax.loc[SH])
                 except KeyError:
-                    print( "WARNING: no taxonomy found for " + SH, file=sys.stderr )
-                    tax = [""]*num_ranks
-                conf = m[1]/100.0
+                    print("WARNING: no taxonomy found for " + SH, file=sys.stderr)
+                    tax = [""] * num_ranks
+                conf = m[1] / 100.0
         if SH != "":
             tax_list = tax[0:num_ranks] + [SH] + [conf]
-            taxtable.loc[ taxtable['ASV_ID'] == prev_ASV, tax_entries] = tax_list
+            taxtable.loc[taxtable["ASV_ID"] == prev_ASV, tax_entries] = tax_list
         prev_ASV = ASV
         maxid = -1
         maxlen = -1
@@ -101,35 +101,35 @@ for row in fh:
         elif pid == maxid and alen == maxlen:
             matches.append([match, pid, alen])
 
-if match != "*":        # Take care of last row/ASV in match file
+if match != "*":  # Take care of last row/ASV in match file
     SH = ""
     tax = ""
     conf = 0.0
     for m in matches:
-        matchparts = m[0].split('|')
+        matchparts = m[0].split("|")
         try:
-            new_SH = seq2sh.loc[ matchparts[1] ][1]
+            new_SH = seq2sh.loc[matchparts[1]][1]
         except KeyError:
-            print( "WARNING: " + matchparts[1] + " not in seq2SH list", file=sys.stderr )
+            print("WARNING: " + matchparts[1] + " not in seq2SH list", file=sys.stderr)
             new_SH = ""
-        if ( pd.isna( new_SH ) ) :
-            print( "WARNING: no SH reported for " + matchparts[1], file=sys.stderr )
+        if pd.isna(new_SH):
+            print("WARNING: no SH reported for " + matchparts[1], file=sys.stderr)
             new_SH = ""
-        if SH != "" and new_SH != SH :
+        if SH != "" and new_SH != SH:
             SH = ""
             tax = ""
             break
         elif new_SH != "":
             SH = new_SH
             try:
-                tax = list(shtax.loc[ SH ])
+                tax = list(shtax.loc[SH])
             except KeyError:
-                print( "WARNING: no taxonomy found for " + SH, file=sys.stderr )
-                tax = [""]*num_ranks
-            conf = m[1]/100.0
+                print("WARNING: no taxonomy found for " + SH, file=sys.stderr)
+                tax = [""] * num_ranks
+            conf = m[1] / 100.0
     if SH != "":
         tax_list = tax[0:num_ranks] + [SH] + [conf]
-        taxtable.loc[ taxtable['ASV_ID'] == prev_ASV, tax_entries] = tax_list
+        taxtable.loc[taxtable["ASV_ID"] == prev_ASV, tax_entries] = tax_list
 
 
 # Write new taxtable, with SH and new taxonomy added if found

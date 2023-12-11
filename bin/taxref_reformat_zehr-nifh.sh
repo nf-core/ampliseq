02a25293d77cc501d01ed87de01013909037cea1
Daniel Lundin <erik.rikard.daniel@gmail.com>
2023-12-11 19:39:53 +0100

    Fix formatting mistake

--- bin/taxref_reformat_zehr-nifh.sh
+++ bin/taxref_reformat_zehr-nifh.sh
@@ -4,4 +4,4 @@
 cp *.fasta assignTaxonomy.fna
 
 # Write the addSpecies() fasta file: addSpecies.fna
-cut -d, -f 2,6,7 *.csv  | grep -v '^sequence,' | sed 's/\(.*\),\(.*\),\(.*\)/>\3 \2\n\1/' > addSpecies.fna
+cut -d, -f 2,6,7 *.csv  | grep -v '^sequence,' | sed 's/\(.*\),[0-9]* \(.*\),\(.*\)/>\3 \2\n\1/' > addSpecies.fna

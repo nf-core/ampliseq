cd30e46561470dafeaf2fdf8aa814a269f60dc95
d4straub <daniel.straub@uni-tuebingen.de>
2024-03-11 13:53:25 +0100

    fix sidle silva ref db

--- bin/taxref_reformat_sidle.sh
+++ bin/taxref_reformat_sidle.sh
@@ -14,8 +14,8 @@ if [ -d "gg_13_8_otus" ]; then
     rm -r gg_13_8_otus
 elif [ -d "SILVA_128_QIIME_release" ]; then
     mv SILVA_128_QIIME_release/rep_set/rep_set_all/${derep}/${derep}_otus.fasta SILVA_128_QIIME_release_rep_set_all_${derep}_otus.seq.fasta
-    gunzip -c /SILVA_128_QIIME_release/rep_set_aligned/${derep}/${derep}_otus_aligned.fasta.gz > SILVA_128_QIIME_release_rep_set_aligned_${derep}_otus_aligned.alnseq.fasta
-    mv SILVA_128_QIIME_release/taxonomy/taxonomy_all/${derep}/consensus_taxonomy_7_levels SILVA_128_QIIME_release_taxonomy_all_${derep}_consensus_taxonomy_7_levels.tax.txt
+    gunzip -c SILVA_128_QIIME_release/rep_set_aligned/${derep}/${derep}_otus_aligned.fasta.gz > SILVA_128_QIIME_release_rep_set_aligned_${derep}_otus_aligned.alnseq.fasta
+    mv SILVA_128_QIIME_release/taxonomy/taxonomy_all/${derep}/consensus_taxonomy_7_levels.txt SILVA_128_QIIME_release_taxonomy_all_${derep}_consensus_taxonomy_7_levels.tax.txt
     # remove uncompressed folder
     rm -r SILVA_128_QIIME_release
 else

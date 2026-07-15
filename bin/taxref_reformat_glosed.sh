ee35794b3948b7ec906fc1f57886c287b38dcd1e
tom-brekke <T.Brekke@kew.org>
2026-07-15 11:40:42 +0100

    updated paths and reformatting for the test dataset of glosed

--- bin/taxref_reformat_glosed.sh
+++ bin/taxref_reformat_glosed.sh
@@ -10,8 +10,20 @@
 
 set -eu
 
+stream_seq() {
+    case "$1" in
+        *.zip) unzip -p "$1" ;;
+        *) gzip -dc "$1" ;;
+    esac
+}
+
 SEQ_GZ="${1:-GloSED__OTU_sequences.fasta.gz}"
 TAX_ZIP="${2:-GloSED__Taxonomy.tsv.zip}"
+
+if [ -z "${1:-}" ] && [ -z "${2:-}" ] && [ ! -f "$SEQ_GZ" ] && [ ! -f "$TAX_ZIP" ] && [ -f "GloSED__OTU_sequences_first1k.fasta.zip" ] && [ -f "GloSED_Taxonomy_first1k.tsv.zip" ]; then
+    SEQ_GZ="GloSED__OTU_sequences_first1k.fasta.zip"
+    TAX_ZIP="GloSED_Taxonomy_first1k.tsv.zip"
+fi
 
 [ -f "$SEQ_GZ" ] || { echo "ERROR: missing $SEQ_GZ" >&2; exit 1; }
 [ -f "$TAX_ZIP" ] || { echo "ERROR: missing $TAX_ZIP" >&2; exit 1; }
@@ -69,12 +81,12 @@ NR==1 {
 }
 ' "$TAX_TSV" > "$META_TSV"
 
-gzip -dc "$SEQ_GZ" | awk -F '\t' 'NR==FNR { tax[$1] = $2; next }
+stream_seq "$SEQ_GZ" | awk -F '\t' 'NR==FNR { tax[$1] = $2; next }
 /^>/ { id = substr($0,2); if (id in tax && tax[id] != "") print ">" tax[id]; else print ">" id; next }
 { print }
 ' "$META_TSV" - > assignTaxonomy.fna
 
-gzip -dc "$SEQ_GZ" | awk -F '\t' 'NR==FNR { if ($3 != "." && $3 != "" && $3 != "NA" && $4 != "." && $4 != "" && $4 != "NA") addsp[$1] = $3 " " $4; next }
+stream_seq "$SEQ_GZ" | awk -F '\t' 'NR==FNR { if ($3 != "." && $3 != "" && $3 != "NA" && $4 != "." && $4 != "" && $4 != "NA") addsp[$1] = $3 " " $4; next }
 /^>/ { id = substr($0,2); keep = (id in addsp); if (keep) print ">" id " " addsp[id]; next }
 { if (keep) print }
 ' "$META_TSV" - > addSpecies.fna
